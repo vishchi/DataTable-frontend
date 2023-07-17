05663e51eeccb254ab612961eb38e38004383aeb
vishchi <vishalsc1999@gmail.com>
2023-07-17 18:41:47 +0530

    Added Loader

--- pages/DataTable.tsx
+++ pages/DataTable.tsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Table, Thead, Tbody, Tr, Th, Td, Button, Select, Flex, TableCaption } from "@chakra-ui/react";
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Button, Select, Flex, TableCaption, Progress } from "@chakra-ui/react";
 import { DataTableProps } from '../types/DataTableTypes';
 
 const DataTable: React.FC<DataTableProps> = ({ sortable, caption, headers, rows }) => {
-    const [sortField, setSortField] = useState('');
-    const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [dataPerPage] = useState(5);
-  
-    useEffect(() => {
-      if (headers && headers.length > 0) {
-        setSortField(headers[0].field || '');
-      }
-    }, [headers]);
+  const [sortField, setSortField] = useState('');
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataPerPage] = useState(5);
+
+  useEffect(() => {
+    if (headers && headers.length > 0) {
+      setSortField(headers[0].field || '');
+    }
+  }, [headers]);
 
   const sortedData = [...rows].sort((a, b) => {
     if (!sortable || !sortField) {
@@ -60,53 +60,57 @@ const DataTable: React.FC<DataTableProps> = ({ sortable, caption, headers, rows 
 
   return (
     <Box overflowX="auto">
-      <Table>
-        <TableCaption fontSize="2xl" fontWeight="bold" placement="top">{caption}</TableCaption>
-        <Thead>
-          <Tr>
-            {headers.map(header => (
-              <Th key={header.field} onClick={() => handleHeaderClick(header.field)} cursor={sortable && header.sortable ? "pointer" : "auto"}>
-                {header.label}
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {currentPosts.map((item) => (
-            <Tr key={item.id}>
+      {rows.length === 0 ? (
+        <Progress size="xs" isIndeterminate />
+      ) : (
+        <Table>
+          <TableCaption fontSize="2xl" fontWeight="bold" placement="top">{caption}</TableCaption>
+          <Thead>
+            <Tr>
               {headers.map(header => (
-                <Td key={header.field}>
-                  {header.field === 'select' ? (
-                    <Select defaultValue={item[header.field].toString()}>
-                      <option value="true">true</option>
-                      <option value="false">false</option>
-                    </Select>
-                  ) : header.field === 'status' ? (
-                    <span style={getStatusStyle(item[header.field])}>
-                      {item[header.field]}
-                    </span>
-                  ) : (
-                    item[header.field]
-                  )}
-                </Td>
+                <Th key={header.field} onClick={() => handleHeaderClick(header.field)} cursor={sortable && header.sortable ? "pointer" : "auto"}>
+                  {header.label}
+                </Th>
               ))}
             </Tr>
-          ))}
-        </Tbody>
-        <tfoot>
-          <Tr>
-            <Td colSpan={headers.length}>
-              <Flex justifyContent="flex-end">
-                <Button onClick={() => changePage(currentPage - 1)} disabled={currentPage === 1}>Previous</Button>
-                {Array.from({ length: totalPages }, (_, index) => (
-                  <Button onClick={() => changePage(index + 1)} key={index + 1}>{index + 1}</Button>
+          </Thead>
+          <Tbody>
+            {currentPosts.map((item) => (
+              <Tr key={item.id}>
+                {headers.map(header => (
+                  <Td key={header.field}>
+                    {header.field === 'select' ? (
+                      <Select defaultValue={item[header.field].toString()}>
+                        <option value="true">true</option>
+                        <option value="false">false</option>
+                      </Select>
+                    ) : header.field === 'status' ? (
+                      <span style={getStatusStyle(item[header.field])}>
+                        {item[header.field]}
+                      </span>
+                    ) : (
+                      item[header.field]
+                    )}
+                  </Td>
                 ))}
-                <Button onClick={() => changePage(currentPage + 1)} disabled={currentPage === totalPages}>Next</Button>
-              </Flex>
-            </Td>
-          </Tr>
-        </tfoot>
-      </Table>
+              </Tr>
+            ))}
+          </Tbody>
+          <tfoot>
+            <Tr>
+              <Td colSpan={headers.length}>
+                <Flex justifyContent="flex-end">
+                  <Button onClick={() => changePage(currentPage - 1)} disabled={currentPage === 1}>Previous</Button>
+                  {Array.from({ length: totalPages }, (_, index) => (
+                    <Button onClick={() => changePage(index + 1)} key={index + 1}>{index + 1}</Button>
+                  ))}
+                  <Button onClick={() => changePage(currentPage + 1)} disabled={currentPage === totalPages}>Next</Button>
+                </Flex>
+              </Td>
+            </Tr>
+          </tfoot>
+        </Table>
+      )}
     </Box>
   );
 };
